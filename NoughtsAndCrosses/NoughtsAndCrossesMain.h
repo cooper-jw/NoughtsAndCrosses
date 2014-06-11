891c0c7077b0b470334fa5fa5f48a95ffc4f5318
Martyn Prigmore <martyn.prigmore@hotmail.com>
2014-06-11 18:22:23 +0100

    Initial commit of starter solution
    Started with DirectX App (Windows)
    Added DirectXTK
    Added BackgroundRenderer, initialised to a beige background
    Modified NoughtsAndCrossesMain class to use the BackgroundRenderer for determining the colour to clear the back buffer to.

---
+++ NoughtsAndCrosses/NoughtsAndCrossesMain.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "Common\StepTimer.h"
+#include "Common\DeviceResources.h"
+#include "Content\BackgroundRenderer.h"
+
+// Renders Direct2D and 3D content on the screen.
+namespace NoughtsAndCrosses
+{
+	class NoughtsAndCrossesMain : public DX::IDeviceNotify
+	{
+	public:
+		NoughtsAndCrossesMain(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		~NoughtsAndCrossesMain();
+		void CreateWindowSizeDependentResources();
+		void Update();
+		bool Render();
+
+		// IDeviceNotify
+		virtual void OnDeviceLost();
+		virtual void OnDeviceRestored();
+
+	private:
+		// Cached pointer to device resources.
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+
+		// TODO: Replace with your own content renderers.
+    BackgroundRenderer m_background;
+
+		// Rendering loop timer.
+		DX::StepTimer m_timer;
+	};
+}
